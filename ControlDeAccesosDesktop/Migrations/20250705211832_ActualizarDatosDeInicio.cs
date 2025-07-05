6c92aa32fbb74065a78c792e597a7495069ee5d7
Alfredo Garcia <s23120180@alumnos.itsur.edu.mx>
2025-07-05 15:20:32 -0600

    Actualizacion de datos insertados al iniciar.

---
+++ ControlDeAccesosDesktop/Migrations/20250705211832_ActualizarDatosDeInicio.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ControlDeAccesosDesktop.Migrations
+{
+    /// <inheritdoc />
+    public partial class ActualizarDatosDeInicio : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Guardias",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "ContrasenaHash", "Nombre", "Usuario" },
+                values: new object[] { "isa123", "Isela Cerritos Guzman", "isaguzman" });
+
+            migrationBuilder.UpdateData(
+                table: "Guardias",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "ContrasenaHash", "Nombre", "Usuario" },
+                values: new object[] { "root", "Alfredo Garcia Olmedo", "root" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Guardias",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "ContrasenaHash", "Nombre", "Usuario" },
+                values: new object[] { "admin123", "Carlos Ramírez", "carlosr" });
+
+            migrationBuilder.UpdateData(
+                table: "Guardias",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "ContrasenaHash", "Nombre", "Usuario" },
+                values: new object[] { "guardia456", "Laura Gómez", "laurag" });
+        }
+    }
+}
